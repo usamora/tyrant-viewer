5eb52739f749a5e88d2adae3abe91f438291b1b8
usamora <69716666+usamora@users.noreply.github.com>
2026-04-23 23:44:40 +0300

    - Apply basic styles to the character list

--- src/events/createCharactersList.ts
+++ src/events/createCharactersList.ts
@@ -10,21 +10,37 @@ export function createCharactersList(
     
     charactersArray.map((item: any) => {
         let listItem = document.createElement('li');
+        listItem.classList.add('entity-block');
 
-        let entityButton = document.createElement('button');
-        entityButton.textContent = item.name;
-        entityButton.setAttribute('type', 'button');
-        entityButton.dataset.charId = item.id;
+        let characterName = document.createElement('span');
+        characterName.classList.add('character-name');
+        characterName.textContent = item.name;
+        listItem.appendChild(characterName);
 
-        entityButton.addEventListener('click', async (e: any) => {
-            let charId = e.target.dataset.charId;
+        const characterVariantsList = document.createElement('ul');
+        characterVariantsList.classList.add('character-variation');
+        listItem.appendChild(characterVariantsList);
 
-            const newModelPath = import.meta.env.BASE_URL + `/assets/chars/${charId}/${charId}_l/${charId}_L.model3.json`;
+        item.variants.map((variant: any) => {
+            let variantListItem = document.createElement('li');      
+            let variantButton = document.createElement('button');
 
-            await loadModel(app, newModelPath);
+            variantButton.setAttribute('type', 'button');
+            variantButton.classList.add('character-variation_button');
+            variantButton.textContent = variant.label;
+            variantButton.dataset.path = variant.path;
+
+            variantListItem.appendChild(variantButton);
+            characterVariantsList.appendChild(variantListItem);
+
+            variantButton.addEventListener('click', async (e: any) => {
+                let modelPath = e.target.dataset.path;
+
+                const newModelPath = import.meta.env.BASE_URL + modelPath;
+
+                await loadModel(app, newModelPath);
+            });
         });
-
-        listItem.appendChild(entityButton);
 
         entityListBlock?.appendChild(listItem);
   })
